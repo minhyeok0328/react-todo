70977c63b3de4e4e4e65253f8ec85d0e2c977f53
minhyeok0328 <alsgur0008@naver.com>
2023-01-30 19:24:40 +0900

    fix: 배열 또는 객체일 경우 값이 같아도 계속 callback 함수 호출되는 문제 수정

--- vanilla/src/core/Observe.js
+++ vanilla/src/core/Observe.js
@@ -1,6 +1,6 @@
 export default class Observe {
   constructor(value, callback) {
-    const proxy = new Proxy(value, {
+    return new Proxy(value, {
       get(target, property) {
         return target[property];
       },
@@ -8,14 +8,12 @@ export default class Observe {
         const oldValue = target[property];
         target[property] = value;
 
-        if (value !== oldValue && callback) {
+        if (JSON.stringify(value) !== JSON.stringify(oldValue) && callback) {
           callback(property, oldValue, value);
         }
 
         return true;
       },
     });
-
-    return proxy;
   }
 }
